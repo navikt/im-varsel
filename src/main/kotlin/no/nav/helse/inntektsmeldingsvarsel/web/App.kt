d763861e79df5130c5c4b7e22e178a888f90b922
tuxbear <ole-andre.riga.johansen@nav.no>
2020-04-17 12:57:04 +0200

    JSON-håndtering for helsesjekk

--- src/main/kotlin/no/nav/helse/inntektsmeldingsvarsel/web/App.kt
+++ src/main/kotlin/no/nav/helse/inntektsmeldingsvarsel/web/App.kt
@@ -1,14 +1,23 @@
 package no.nav.helse.inntektsmeldingsvarsel.web
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.typesafe.config.ConfigFactory
+import io.ktor.application.install
 import io.ktor.config.HoconApplicationConfig
+import io.ktor.features.ContentNegotiation
+import io.ktor.http.ContentType
+import io.ktor.jackson.JacksonConverter
 import io.ktor.server.engine.applicationEngineEnvironment
 import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.util.KtorExperimentalAPI
+import no.nav.helse.inntektsmeldingsvarsel.dependencyinjection.selectModuleBasedOnProfile
+import no.nav.helse.inntektsmeldingsvarsel.nais.nais
 import no.nav.helse.inntektsmeldingsvarsel.varsling.SendVarslingJob
 import no.nav.helse.inntektsmeldingsvarsel.varsling.mottak.VarslingsmeldingProcessor
+import org.koin.ktor.ext.Koin
+import org.koin.ktor.ext.get
 import org.koin.ktor.ext.getKoin
 import org.slf4j.LoggerFactory
 
@@ -49,7 +58,16 @@ fun createApplicationEnvironment() = applicationEngineEnvironment {
     }
 
     module {
-        spionModule(config)
+        install(Koin) {
+            modules(selectModuleBasedOnProfile(config))
+        }
+
+        install(ContentNegotiation) {
+            val commonObjectMapper = get<ObjectMapper>()
+            register(ContentType.Application.Json, JacksonConverter(commonObjectMapper))
+        }
+
+        nais()
     }
 }
 
