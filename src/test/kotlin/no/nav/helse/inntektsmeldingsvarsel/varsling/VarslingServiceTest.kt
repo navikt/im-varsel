bc16713506a0e293aa9e5c3f0b4fdd08547876b7
tuxbear <ole-andre.riga.johansen@nav.no>
2020-11-18 10:49:23 +0100

    Legger til grafana-metrikk for bortfiltrerte varslinger

--- src/test/kotlin/no/nav/helse/inntektsmeldingsvarsel/varsling/VarslingServiceTest.kt
+++ src/test/kotlin/no/nav/helse/inntektsmeldingsvarsel/varsling/VarslingServiceTest.kt
@@ -3,6 +3,7 @@ package no.nav.helse.inntektsmeldingsvarsel.varsling
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.zaxxer.hikari.HikariDataSource
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
@@ -15,28 +16,22 @@ import no.nav.helse.inntektsmeldingsvarsel.domene.varsling.repository.VentendeBe
 import no.nav.helse.inntektsmeldingsvarsel.domene.varsling.repository.VarslingDbEntity
 import no.nav.helse.inntektsmeldingsvarsel.domene.varsling.repository.VarslingRepository
 import no.nav.helse.inntektsmeldingsvarsel.varsling.mottak.SpleisInntektsmeldingMelding
+import no.nav.helse.inntektsmeldingsvarsel.varsling.mottak.SpleisInntektsmeldingMelding.Meldingstype.TRENGER_IKKE_INNTEKTSMELDING
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.time.LocalDateTime
-import javax.sql.DataSource
 
 internal class VarslingServiceTest {
-    val existingVarselAggregat = Varsling(
-            "123456785",
-            mutableSetOf()
-    )
-
-    val pdlPerson = PdlPerson(listOf(PdlPersonNavn("Navn", null, "Navnesen")))
-
-    val mappingResultDto = VarslingDbEntity(data = "[]", uuid = "test", read = false,
-            sent = false, opprettet = LocalDateTime.now(), behandlet = LocalDateTime.now(), virksomhetsNr = "12345")
-
     val allowMock = mockk<AllowList>()
     val varselRepo = mockk<VarslingRepository>(relaxed = true)
     val ventendeRepoMock = mockk<VentendeBehandlingerRepository>(relaxed = true)
     val altinnVarselMapperMock = mockk<VarslingMapper>()
-    val datasourceMock = mockk<DataSource>()
-    val pdlClientMock = mockk<PdlClient>()
+    val datasourceMock = mockk<HikariDataSource>(relaxed = true)
+
+    val pdlClientMock = mockk<PdlClient>() {
+        every { person(any()) } returns PdlPerson(listOf(PdlPersonNavn("Navn", null, "Navnesen")))
+    }
+
     val objectMapper = ObjectMapper().registerModule(KotlinModule()).registerModule(JavaTimeModule())
 
     val serviceUnderTest = VarslingService(
@@ -49,7 +44,7 @@ internal class VarslingServiceTest {
             allowMock
     )
 
-    private val varsling = SpleisInntektsmeldingMelding(
+    private val msg_mangler = SpleisInntektsmeldingMelding(
              "123456785",
             LocalDate.now(),
             LocalDate.now().plusDays(1),
@@ -58,14 +53,32 @@ internal class VarslingServiceTest {
     )
 
     @Test
-    fun `Ignorerer meldinger ang org-nummer som ikke er i allow list`() {
-        every { allowMock.isAllowed(varsling.organisasjonsnummer) } returns false
+    fun `Innkommende meldinger fra Spleis blir lagret i ventende tabellen`() {
+        serviceUnderTest.handleMessage(objectMapper.writeValueAsString(msg_mangler))
 
-        serviceUnderTest.handleMessage(objectMapper.writeValueAsString(varsling))
-
-        verify(exactly = 1) { allowMock.isAllowed(varsling.organisasjonsnummer) }
-        verify(exactly = 0) { ventendeRepoMock.insertIfNotExists(any(), any(), any(), any(), any()) }
-        verify(exactly = 0) { varselRepo.updateData(any(), any()) }
+        verify(exactly = 1) { ventendeRepoMock.insertIfNotExists(msg_mangler.fødselsnummer, msg_mangler.organisasjonsnummer, msg_mangler.fom, msg_mangler.tom, msg_mangler.opprettet) }
+        verify(exactly = 0) { varselRepo.insert(any(), any()) }
     }
 
+    @Test
+    fun `Melding om at man ikke lenger mangler IM fjerner ventende melding`() {
+        val trengerIkke = msg_mangler.copy(meldingsType = TRENGER_IKKE_INNTEKTSMELDING)
+        serviceUnderTest.handleMessage(objectMapper.writeValueAsString(trengerIkke))
+
+        verify(exactly = 1) { ventendeRepoMock.remove(trengerIkke.fødselsnummer, trengerIkke.organisasjonsnummer, trengerIkke.fom, any()) }
+        verify(exactly = 0) { ventendeRepoMock.insertIfNotExists(msg_mangler.fødselsnummer, msg_mangler.organisasjonsnummer, msg_mangler.fom, msg_mangler.tom, msg_mangler.opprettet) }
+        verify(exactly = 0) { varselRepo.insert(any(), any()) }
+    }
+
+    @Test
+    fun `Oppretter ikke varsler og fjerner ventende melding ved org-nummer som ikke er i allow list`() {
+        every { allowMock.isAllowed(msg_mangler.organisasjonsnummer) } returns false
+        every { ventendeRepoMock.findOlderThan(any()) } returns setOf(msg_mangler)
+
+        serviceUnderTest.opprettVarslingerFraVentendeMeldinger()
+
+        verify(exactly = 1) { allowMock.isAllowed(msg_mangler.organisasjonsnummer) }
+        verify(exactly = 1) { ventendeRepoMock.remove(msg_mangler.fødselsnummer, msg_mangler.organisasjonsnummer, msg_mangler.fom, any()) }
+        verify(exactly = 0) { varselRepo.insert(any(), any()) }
+    }
 }
