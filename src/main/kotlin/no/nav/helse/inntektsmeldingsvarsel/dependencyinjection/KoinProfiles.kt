a3c0b7b749246ba9c44562ef32bab5fea1370cef
tuxbear <ole@wanda.space>
2021-09-29 15:23:05 +0200

    Skrur på utsending i preprod

--- src/main/kotlin/no/nav/helse/inntektsmeldingsvarsel/dependencyinjection/KoinProfiles.kt
+++ src/main/kotlin/no/nav/helse/inntektsmeldingsvarsel/dependencyinjection/KoinProfiles.kt
@@ -209,7 +209,7 @@ fun preprodConfig(config: ApplicationConfig) = module {
 
     single { VarslingMapper(get()) }
 
-/*    single {
+    single {
         AltinnVarselSender(
                 get(),
                 AltinnVarselMapper(config.getString("altinn_melding.service_id")),
@@ -218,9 +218,9 @@ fun preprodConfig(config: ApplicationConfig) = module {
                 config.getString("altinn_melding.username"),
                 config.getString("altinn_melding.password")
         ) as VarslingSender
-    }*/
+    }
 
-    single { MockVarslingSender(get()) as VarslingSender }
+    // single { MockVarslingSender(get()) as VarslingSender }
 
     single { DokarkivKlientImpl(config.getString("dokarkiv.base_url"), get(), get()) as DokarkivKlient }
     single {
