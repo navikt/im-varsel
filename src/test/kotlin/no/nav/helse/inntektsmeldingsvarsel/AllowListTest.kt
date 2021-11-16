e17f1cfe63649c641ccea17bc6b60b5ef5b58661
Jonas Enge <jonas.maccyber.enge@nav.no>
2021-11-16 12:00:02 +0100

    Legger til flere tester på AllowList

--- src/test/kotlin/no/nav/helse/inntektsmeldingsvarsel/AllowListTest.kt
+++ src/test/kotlin/no/nav/helse/inntektsmeldingsvarsel/AllowListTest.kt
@@ -1,14 +1,24 @@
 package no.nav.helse.inntektsmeldingsvarsel
 
-
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class AllowListTest {
     @Test
-    internal fun `PilotAllow tillater ikke hva som helst`() {
+    internal fun `AllowAll tillater hva som helst`() {
         val allowAll = AllowAll()
-        assertThat(allowAll.isAllowed("12341234")).isTrue()
+        assertThat(allowAll.isAllowed("12341234")).isTrue
     }
 
+    @Test
+    internal fun `PilotAllow tillater "riktig" organiasjonsnummer`() {
+        val pilotAllowList = PilotAllowList(setOf('1'))
+        assertThat(pilotAllowList.isAllowed("12345134")).isTrue
+    }
+
+    @Test
+    internal fun `PilotAllow tillater ikke "feil" organiasjonsnummer`() {
+        val pilotAllowList = PilotAllowList(setOf('1'))
+        assertThat(pilotAllowList.isAllowed("12345234")).isFalse
+    }
 }
