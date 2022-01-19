9b87a81f10590edb94b46f3e78e656722b37877f
Gustav Berggren <gustav.berggren@nav.no>
2022-01-19 11:13:57 +0100

    POH-1169: Oppgrader felles backend

--- build.gradle.kts
+++ build.gradle.kts
@@ -108,7 +108,7 @@ dependencies {
 
     implementation("com.zaxxer:HikariCP:$hikariVersion")
     implementation("no.nav:vault-jdbc:$vaultJdbcVersion")
-    implementation("no.nav.helsearbeidsgiver:helse-arbeidsgiver-felles-backend:2021.08.27-15-00-1d672")
+    implementation("no.nav.helsearbeidsgiver:helse-arbeidsgiver-felles-backend:2022.01.18-08-47-f6aa0")
     implementation("no.nav.common:log:2.2020.10.15_11.43-b1f02e7bd6ae")
 
     implementation("com.github.tomakehurst:wiremock-standalone:2.25.1")
