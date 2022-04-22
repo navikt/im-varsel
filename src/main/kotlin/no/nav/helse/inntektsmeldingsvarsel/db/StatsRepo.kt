c706b695865c0e71992037abd2fc760071e97f30
Gustav Berggren <83211434+b162214@users.noreply.github.com>
2022-04-22 12:14:17 +0200

    lest tidspunkt sql (#43)

--- src/main/kotlin/no/nav/helse/inntektsmeldingsvarsel/db/StatsRepo.kt
+++ src/main/kotlin/no/nav/helse/inntektsmeldingsvarsel/db/StatsRepo.kt
@@ -16,15 +16,27 @@ class StatsRepoImpl(
 ) : IStatsRepo {
     override fun getVarselStats(): List<VarselStats> {
         val query = """
-            SELECT
-                extract('week' from date_trunc('week',behandlet)) as uke,
-                extract('year' from date_trunc('week',behandlet)) as year,
-                count(*) filter( WHERE sent = true) as sent,
-                count(*) filter( WHERE read=true and sent = true) as lest
-            from varsling
-            where behandlet > NOW()::DATE - INTERVAL '12 MONTHS'
-            group by  extract('year' from date_trunc('week',behandlet)), extract('week' from date_trunc('week',behandlet))
-            order by  extract('year' from date_trunc('week',behandlet)), extract('week' from date_trunc('week',behandlet));
+            SELECT s.year, s.uke, coalesce(lest, 0) as lest, sent FROM
+              (SELECT extract('week' from date_trunc('week', lesttidspunkt)) as uke,
+                      extract('year' from date_trunc('week', lesttidspunkt)) as year,
+                      count(*) filter ( WHERE read = true and sent = true)   as lest
+               from varsling
+               where lesttidspunkt > NOW()::DATE - INTERVAL '12 MONTHS'
+               group by extract('year' from date_trunc('week', lesttidspunkt)),
+                        extract('week' from date_trunc('week', lesttidspunkt))
+               order by extract('year' from date_trunc('week', lesttidspunkt)),
+                        extract('week' from date_trunc('week', lesttidspunkt))
+              ) as l
+            full join
+            (SELECT
+                extract ('week' from date_trunc('week', behandlet)) as uke,
+                extract ('year' from date_trunc('week', behandlet)) as year,
+                count(*) filter ( WHERE sent = true) as sent
+                from varsling
+                where behandlet > NOW()::DATE - INTERVAL '12 MONTHS'
+                group by extract ('year' from date_trunc('week', behandlet)), extract ('week' from date_trunc('week', behandlet))
+                order by extract ('year' from date_trunc('week', behandlet)), extract ('week' from date_trunc('week', behandlet))
+                ) as s on l.uke = s.uke and l.year = s.year;
         """.trimIndent()
 
         ds.connection.use {
